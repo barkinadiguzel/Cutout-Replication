ffb8c0cc4d9990101925c851077e6b09e8adb51c
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2025-12-29 11:25:46 +0300

    Create conv_block.py

---
+++ src/layers/conv_block.py
@@ -0,0 +1,11 @@
+import torch.nn as nn
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_ch, out_ch, kernel_size=3, stride=1, padding=1):
+        super().__init__()
+        self.conv = nn.Conv2d(in_ch, out_ch, kernel_size, stride, padding, bias=False)
+        self.bn = nn.BatchNorm2d(out_ch)
+        self.act = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        return self.act(self.bn(self.conv(x)))
